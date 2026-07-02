55cc60918280f443dcfd75b280690e6d3b6bb01d
Albin-or <albinoropezadev@gmail.com>
2026-07-01 20:31:56 -0400

    fixed build issues and implemented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { HomePage } from './pages/HomePage.jsx'
-import { ItemPage } from './pages/ItemPage.jsx'
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar.jsx'
 import { FiltersProvider } from './components/FiltersContext.jsx'
 import { Routes, Route } from 'react-router'
 import { NotFoundPage } from './pages/NotFoundPage.jsx'
-import { AddItemPage } from './pages/AddItemPage.jsx'
-import { RestockPage } from './pages/RestockPage.jsx'
-import { DischargePage } from './pages/DischargePage.jsx'
 import { InventoryProvider } from './hooks/useInventory.jsx'
+import { lazy, Suspense } from 'react'
+
+const HomePage = lazy(() => import('./pages/HomePage.jsx').then(module => ({ default: module.HomePage })))
+const ItemPage = lazy(() => import('./pages/ItemPage.jsx').then(module => ({ default: module.ItemPage })))
+const AddItemPage = lazy(() => import('./pages/AddItemPage.jsx').then(module => ({ default: module.AddItemPage })))
+const RestockPage = lazy(() => import('./pages/RestockPage.jsx').then(module => ({ default: module.RestockPage })))
+const DischargePage = lazy(() => import('./pages/DischargePage.jsx').then(module => ({ default: module.DischargePage })))
 
 function App() {
   return (
@@ -17,14 +19,16 @@ function App() {
         <header className={styles.headerSticky}>
           <Navbar />
         </header>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product" element={<ItemPage />} />
-          <Route path="/addItem" element={<AddItemPage />} />
-          <Route path="/restock" element={<RestockPage />} />
-          <Route path="/discharge" element={<DischargePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <Suspense fallback={<div>Cargando...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product" element={<ItemPage />} />
+            <Route path="/addItem" element={<AddItemPage />} />
+            <Route path="/restock" element={<RestockPage />} />
+            <Route path="/discharge" element={<DischargePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </FiltersProvider>
     </InventoryProvider>
   );
